8a3dee018c680f7b860aeb0b830bd9baf96c39fe
Vigno04 <davivigna2004@gmail.com>
2026-08-07 19:18:34 +0200

    Bump version to 0.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sharkord.android"
         minSdk = 28
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.1.5"
+        versionCode = 7
+        versionName = "0.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
