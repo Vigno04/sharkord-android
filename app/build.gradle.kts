6a8a19a045e6327f54f9d4c6446a0dc2b692f41d
Vigno04 <davivigna2004@gmail.com>
2026-08-08 18:01:25 +0200

    Fix UI deprecation warnings and bump version to v0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sharkord.android"
         minSdk = 28
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.1.5"
+        versionCode = 7
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
